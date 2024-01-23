6dab823b094ba72c8e5a133fc837a71261adb594
yurapwnz <yurapwnz1992@ya.ru>
2024-01-23 19:44:55 +0300

    add select datepicker

--- test_automation_practice_form.py
+++ test_automation_practice_form.py
@@ -15,8 +15,22 @@ def test_should_final_form_text():
     browser.element('#uploadPicture').type(current_dir)
     browser.element('#currentAddress').type('Moscow, Russia')
     browser.element('#subjectsInput').type('English').press_enter()
-    browser.element('#dateOfBirthInput').type(
-        '01 May 1990')
+    # Open the date picker
+    date_picker = browser.element('#dateOfBirthInput')
+    date_picker.click()
+    # Select the year
+    year_dropdown = browser.element(".react-datepicker__year-select")
+    year_dropdown.click()
+    year_option = year_dropdown.element('//option[@value="1990"]')
+    year_option.click()
+    # Select the month
+    month_dropdown = browser.element('.react-datepicker__month-select')
+    month_dropdown.click()
+    month_option = month_dropdown.element('//option[@value="4"]')  # May is the 5th month, but indexing starts at 0
+    month_option.click()
+    # Select the day
+    day = browser.all('.react-datepicker__day').filtered_by(have.exact_text('1')).first()
+    day.click()
     browser.element('#react-select-3-input').type('NCR').press_enter()
     browser.element('#react-select-4-input').type('Delhi').press_enter()
     browser.element('#submit').click()
