ccdbd4d2d75e4821df97484dab36dc302e43f354
yurapwnz <yurapwnz1992@ya.ru>
2024-02-22 14:44:36 +0300

    fix config

--- project.py
+++ project.py
@@ -7,7 +7,7 @@ BrowserType = Literal['chrome', 'firefox', 'edge']
 class Config(pydantic.BaseSettings):
 
     base_url: str = 'https://demoqa.com/automation-practice-form'
-    window_width: int = 1800
-    window_height: int = 1000
+    window_width: int = 1920
+    window_height: int = 1080
     timeout: float = 5
     driver_name: BrowserType = 'chrome'
